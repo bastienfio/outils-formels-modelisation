beee4bad1eeeec34c34f24d665965600d988751b
bastienfio <bastien.fioretti@live.fr>
2017-10-11 17:53:37 +0200

    TP 1 première partie OK

--- tp-01/TaskManager/Sources/TaskManager/main.swift
+++ tp-01/TaskManager/Sources/TaskManager/main.swift
@@ -1,11 +1,33 @@
 import TaskManagerLib
+import PetriKit
 
 let taskManager = createTaskManager()
 
+let create = taskManager.transitions.filter{$0.name == "create"}[0]
+let spawn = taskManager.transitions.filter{$0.name == "spawn"}[0]
+let exec = taskManager.transitions.filter{$0.name == "exec"}[0]
+let success = taskManager.transitions.filter{$0.name == "success"}[0]
+let fail = taskManager.transitions.filter{$0.name == "fail"}[0]
+let taskPool = taskManager.places.filter{$0.name == "taskPool"}[0]
+let processPool = taskManager.places.filter{$0.name == "processPool"}[0]
+let inProgress = taskManager.places.filter{$0.name == "inProgress"}[0]
+
 // Show here an example of sequence that leads to the described problem.
 // For instance:
-//     let m1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0])
-//     let m2 = spawn.fire(from: m1!)
+
+var m: PTMarking = [taskPool: 0, processPool: 0, inProgress: 0]
+for t in [create, spawn, spawn, exec, exec, success, create, spawn, spawn, exec, exec, success] {
+    m = t.fire(from: m)!
+
+    print("Etape", t, m)
+  }
+  print("On a donc bien un proccessus qui n'est pas détruit avec cette execution")
+
+
+    //let m1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0])
+    //let m2 = spawn.fire(from: m1!)
+
+    //print(m1)
 //     ...
 
 let correctTaskManager = createCorrectTaskManager()
